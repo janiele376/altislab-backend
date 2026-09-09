55636d912309a86d5dcad6606ce1e1ada41c5bcb
Janiele <janiele.nascimento.2426@gmail.com>
2026-09-09 13:44:11 -0300

    feat: repository complete

---
+++ src/main/java/com/altis/library_backend/users/repositories/UserRepository.java
@@ -0,0 +1,19 @@
+package com.altis.library_backend.users.repositories;
+
+import com.altis.library_backend.users.models.entities.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<Users, Long> {
+    Optional<Users> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    Optional<Users> findByCpf(String cpf);
+
+    boolean existsByCpf(String cpf);
+
+}
